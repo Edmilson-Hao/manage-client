490fbf7c977d60d689067f06ec005edc1c418267
edmilson <edmilsonwp>
2020-12-25 11:52:26 -0300

    Ta indo

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 window.addEventListener('load', function () {
-    const body = document.body;
-    body.style.backgroundSize = 'cover';
+    const appContainer = document.querySelector('app')
+    const appLogoImage = document.querySelector('.icon-logo')
+    const notAUserImage = document.querySelector('.not-a-user-icon')
+
     if (screen.width > screen.height) {
-        body.style.backgroundImage = "url('./src/assets/desktop-splash-screen.png')"
-        document.width = screen.height/2;
-    } else if (screen.width < screen.height) body.style.backgroundImage = "url('./src/assets/mobile-splash-screen.png')";
+        appContainer.style.width = '75vh'
+        appContainer.style.height = '100vh'
+    } else if (screen.width < screen.height) {
+        appContainer.style.width = '100vw'
+        appContainer.style.height = '92.5vh'
+
+        appLogoImage.style.position = 'absolute'
+        appLogoImage.style.top = '60vh'
+        appLogoImage.style.left = '20vw'
+
+        notAUserImage.style.width = '30vw'
+        notAUserImage.style.height = '30vw'
+        notAUserImage.style.left = '35vw'
+        notAUserImage.style.position = 'absolute'
+    }
 })
 
+/*
 firebase.auth().onAuthStateChanged(function(user) {
     if (!user) window.location.href = './login.html';
 })
-
+*/
